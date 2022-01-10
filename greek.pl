3f0eedddd4648d1f49ad0c94ed66f5a63bbc6e98
Stephan Kämper <the.tester@seasidetesting.com>
2022-01-10 12:33:18 +0100

    Adds queries so that running the code in IntelliJ produces some output
    Signed-off-by: Stephan Kämper <the.tester@seasidetesting.com>

--- greek.pl
+++ greek.pl
@@ -1,4 +1,7 @@
 is_human(socrates).
 
-is_mortal(Being) :- is_human(Being), format("~a is mortal", Being).
-is_mortal(Being) :- not(is_human(Being)), format("~a may or may not be mortal", Being).
+is_mortal(Being) :- is_human(Being), format("~a is mortal~n", Being).
+is_mortal(Being) :- not(is_human(Being)), format("A/an ~a may or may not be mortal~n", Being).
+
+?- is_mortal(socrates).
+?- is_mortal(thing).
